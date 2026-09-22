88f9b75b1a71bee12cea484c7eaa054c83b24d1e
Erland Lindmark <erland@isaksson.info>
2026-09-22 19:22:25 +0200

    Remove implicit-account survey import

--- backend/src/main/java/info/isaksson/erland/survey/importing/SurveyImportService.java
+++ backend/src/main/java/info/isaksson/erland/survey/importing/SurveyImportService.java
@@ -24,17 +24,6 @@ public class SurveyImportService {
     @Inject SurveyService surveyService;
 
     @Transactional
-    public SurveyView importDefinition(UUID userId, SurveyDefinitionExport document) {
-        validateEnvelope(document);
-        var definition = document.survey();
-        List<QuestionInput> questions = definition.questions() == null ? List.of() : definition.questions().stream()
-                .map(this::question)
-                .toList();
-        return surveyService.create(userId,
-                new SurveyInput(definition.title(), definition.description(), SurveyStatus.DRAFT, questions));
-    }
-
-    @Transactional
     public SurveyView importDefinition(UUID userId, UUID accountId, SurveyDefinitionExport document) {
         validateEnvelope(document);
         var definition = document.survey();
